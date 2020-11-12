35f12b969a9fa267234347609c3f3d01bf69c8cd
Jessie Chen <yhcjessie1@gmail.com>
2020-11-12 10:52:13 -0500

    add button and click function

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ class App extends Component {
       tasks:props.tasks,//a copy of the task inherit from parent
     }
   }
+  //Click function refer line 25
+  markAllDone=(event)=>{
+    console.group('it works!!!');
+  }
   render() { 
     return (
     <>
@@ -15,7 +19,10 @@ class App extends Component {
         <h1 className="container">To-Do List</h1>
         <hr/>
         {/* refering to line 8 for STATE */}
+        {/* use Broswer component to remove items in state */}
         <TasksList tasks={this.state.tasks}/>
+        {/* referece to line 11 for function click */}
+        <button onClick={this.markAllDone}>Mark All Done</button>
         {/* <TasksList tasks={this.props.tasks}/> */}
       </div>
     </>
