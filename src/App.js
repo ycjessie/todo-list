1aede576accc10590c02fb2c2911c528cf6835aa
Jessie Chen <yhcjessie1@gmail.com>
2020-11-12 10:48:15 -0500

    Use State to copy props to state

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import React, { Component } from 'react';
 import TasksList from './TasksList';
 import './App.css'
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      tasks:props.tasks,//a copy of the task inherit from parent
+    }
+  }
   render() { 
     return (
     <>
-      <h1>To-Do List</h1>
-      <hr/>
-      <TasksList tasks={this.props.tasks}/>
+      <div>
+        <h1 className="container">To-Do List</h1>
+        <hr/>
+        {/* refering to line 8 for STATE */}
+        <TasksList tasks={this.state.tasks}/>
+        {/* <TasksList tasks={this.props.tasks}/> */}
+      </div>
     </>
   )
   }
